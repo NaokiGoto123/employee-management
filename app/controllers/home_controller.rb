39aae65605854c35f1919dbf6b2314edc7274ab7
Naoki Goto <baron.naoki.g@gmail.com>
2023-06-20 16:36:43 +0900

    set user in before action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_user
+
     def index
+    end
+
+    private
+
+    def set_user
+        @user = current_user
     end
 end
