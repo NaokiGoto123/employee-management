3c972353d6cfb668a0cee5cd55d4a4dee31216da
Naoki Goto <baron.naoki.g@gmail.com>
2023-06-23 18:29:39 +0900

    change param to @user.id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def attendances
         @user = User.find(params[:id])
-        @attendances = Attendance.where(params[:id])
+        @attendances = Attendance.where(user_id: @user.id)
     end
 end
