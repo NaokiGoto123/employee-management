e7c5d587518656d62499e678457f7c43a4857c22
Naoki Goto <baron.naoki.g@gmail.com>
2023-06-23 18:26:31 +0900

    set role staff when registrating

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :set_default_role
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -9,4 +11,10 @@ class User < ApplicationRecord
   belongs_to :manager, class_name: "User", foreign_key: "manager_id", optional: true
 
   has_many :attendances
+
+  private
+
+  def set_default_role
+    self.role ||= :staff
+  end
 end
