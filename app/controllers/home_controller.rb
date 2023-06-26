a6968f7ca659e9329b0f7a72d269d9da58350e40
Naoki Goto <baron.naoki.g@gmail.com>
2023-06-26 14:39:29 +0900

    get attendances

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     before_action :set_user
 
     def index
+        @attendances = current_user.attendances.where(date: Date.today.beginning_of_month..Date.today.end_of_month)
     end
 
     private
