947deae561ff2b7ad3ba9760a20a83c50aa33c89
Naoki Goto <baron.naoki.g@gmail.com>
2023-06-26 14:38:00 +0900

    cant submit if didn't submit for the previous day

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,4 +4,21 @@ class Attendance < ApplicationRecord
   enum absence_type: { sickness: 0, vacation: 1, personal: 2 }
 
   validates :absence_type, presence: true, unless: :worked
+
+  validates :date, uniqueness: { scope: :user_id, message: "You have already created an attendance for today." }
+
+  validate :previous_day_attendance
+
+  private
+
+  def previous_day_attendance
+    # Skip validation for the first day of attendance
+    return if date == user.created_at.to_date
+
+    previous_day = date - 1.day
+
+    unless user.attendances.where(date: previous_day).exists?
+      errors.add(:date, "can't be created because the attendance for the previous day is missing.")
+    end
+  end
 end
